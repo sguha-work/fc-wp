8de7d25b4a8e93bc902be6bc39eb9f8576bcf71c
sguha-work <sahasrangshu@fusioncharts.com>
2015-09-16 13:10:27 +0530

    bug fixes

--- fusioncharts.php
+++ fusioncharts.php
@@ -32,7 +32,11 @@
                 $this->chartOptions["dataSource"] = $chartDataSource;
             } else {
                 $this->chartOptions["dataSource"] = "__dataSource__";
-                $this->chartDataSource = stripslashes($chartDataSource);
+                if($this->chartOptions["dataFormat"] == "json") {
+                    $this->chartDataSource = str_replace("\n", "", stripslashes($chartDataSource));
+                } else {
+                    $this->chartDataSource = str_replace("\n", "", $chartDataSource);
+                }
             }
         }
         
@@ -40,7 +44,11 @@
             if(strpos($this->chartOptions["dataFormat"], "url")) {
                 $outputHTML = str_replace("__constructorOptions__", json_encode($this->chartOptions), $this->constructorTemplate).str_replace("__chartId__", $this->chartOptions["id"], $this->renderTemplate);    
             } else {
-                $outputHTML = str_replace("__constructorOptions__", str_replace("\"__dataSource__\"",$this->chartDataSource,json_encode($this->chartOptions)), $this->constructorTemplate).str_replace("__chartId__", $this->chartOptions["id"], $this->renderTemplate);                    
+                if($this->chartOptions["dataFormat"] == "json") {
+                    $outputHTML = str_replace("__constructorOptions__", str_replace("\"__dataSource__\"",$this->chartDataSource,json_encode($this->chartOptions)), $this->constructorTemplate).str_replace("__chartId__", $this->chartOptions["id"], $this->renderTemplate);                    
+                } else {
+                    $outputHTML = str_replace("__constructorOptions__", str_replace("__dataSource__",$this->chartDataSource,json_encode($this->chartOptions)), $this->constructorTemplate).str_replace("__chartId__", $this->chartOptions["id"], $this->renderTemplate);                    
+                }
             }
             return $outputHTML;
         }
